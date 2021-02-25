00f2ff4f0a37311701b06b6e2a0fca1d18b494fb
Vasiliy Kupriakov <rublag-ns@yandex.ru>
2021-02-26 00:02:51 +0300

    Fix meta

--- src/todo1/views/layout.clj
+++ src/todo1/views/layout.clj
@@ -4,7 +4,7 @@
 (defn render [html]
   (page/html5
    [:head
-    [:meta {:charset "utf-8"}
-     (page/include-css "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css")]]
+    [:meta {:charset "utf-8"}]
+    (page/include-css "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css")]
    [:body
     html]))
