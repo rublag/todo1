1aea7fba8651bef8c27069e6768ef03f971ef84a
Vasiliy Kupriakov <rublag-ns@yandex.ru>
2021-02-25 23:05:23 +0300

    Add layout

---
+++ src/todo1/views/layout.clj
@@ -0,0 +1,10 @@
+(ns todo1.views.layout
+  (:require [hiccup.page :as page]))
+
+(defn render [html]
+  (page/html5
+   [:head
+    [:meta {:charset "utf-8"}
+     (page/include-css "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css")]]
+   [:body
+    html]))
