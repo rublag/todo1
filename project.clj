52a735d0859d7ff676147c9e2fc0ca47e4971dbe
Vasiliy Kupriakov <rublag-ns@yandex.ru>
2021-02-25 23:05:52 +0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -3,11 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [
-                 [ring/ring-core "1.8.2"]
-                 [org.clojure/clojure "1.10.1"]
+  :dependencies [[environ "1.2.0"]
+                 [formative "0.8.11"]
+                 [hiccup "1.0.5"]
+                 [honeysql "1.0.461"]
                  [http-kit "2.5.1"]
-                 [hiccup "1.0.5"]]
+                 [org.clojure/clojure "1.10.1"]
+                 [org.clojure/java.jdbc "0.7.12"]
+                 [org.postgresql/postgresql "42.2.19"]
+                 [ring/ring-core "1.8.2"]]
   :main ^:skip-aot todo1.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
